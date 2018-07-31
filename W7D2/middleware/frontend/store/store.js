84b4d8b0382e2c3c411d7925ef1542695b786567
Franklin Chang <frnklnchng@gmail.com>
2018-07-30 22:00:11 -0700

    Initial commit

---
+++ W7D2/middleware/frontend/store/store.js
@@ -0,0 +1,24 @@
+import { createStore } from 'redux';
+import rootReducer from '../reducers/root_reducer';
+
+const configureStore = (preloadedState = {}) => {
+  const store = createStore(rootReducer, preloadedState);
+  store.subscribe(() => {
+    localStorage.state = JSON.stringify(store.getState());
+  });
+  return store;
+}
+
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  next(action);
+  console.log(store.getState());
+};
+
+const anotherMiddleware = store => next => action => {
+  console.log("Testing");
+  next(action);
+};
+
+export default configureStore;
